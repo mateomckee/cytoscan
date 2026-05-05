df4d62e3c89b5a9cbcd99eacf525babd13dd3117
mateomckee <mateomckee64@gmail.com>
2026-05-05 01:41:08 -0500

    Added unit tests

--- src/cytoscan/__init__.py
+++ src/cytoscan/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.1.0"
+from importlib.metadata import PackageNotFoundError, version as _pkg_version
+
+try:
+    __version__ = _pkg_version("cytoscan")
+except PackageNotFoundError:  # running from source without install
+    __version__ = "0.0.0+unknown"
